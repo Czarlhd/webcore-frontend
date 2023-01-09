ef4553c2f31be1633e02e7a6e0da4fd96407306b
Czarlhd <cesar-lahoud@hotmail.com>
2023-01-09 17:03:07 -0500

    created basic json to svg converter check comments

---
+++ src/assets/CDM_details.js
@@ -0,0 +1,194 @@
+export const ONE_CLASS = {
+	eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//ClassDiagram",
+	_id: "null",
+	name: "1CLASS",
+	classes: [
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//Class",
+			_id: "1",
+			name: "X",
+		},
+	],
+	types: [
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDVoid",
+			_id: "2",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDAny",
+			_id: "3",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDBoolean",
+			_id: "4",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDDouble",
+			_id: "5",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDInt",
+			_id: "6",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDLong",
+			_id: "7",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDString",
+			_id: "8",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDByte",
+			_id: "9",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDFloat",
+			_id: "10",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDChar",
+			_id: "11",
+		},
+	],
+	layout: {
+		_id: "12",
+		containers: [
+			{
+				_id: "13",
+				key: "null",
+				value: [
+					{
+						_id: "14",
+						key: "1",
+						value: {
+							_id: "15",
+							x: 365.5,
+							y: 80.0,
+						},
+					},
+				],
+			},
+		],
+	},
+};
+
+export const TWO_CLASSES_WITH_ASSOCIATION = {
+	eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//ClassDiagram",
+	_id: "null",
+	name: "2CLASSES_WITH_ASSOCIATION",
+	classes: [
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//Class",
+			_id: "1",
+			name: "A",
+			associationEnds: [
+				{
+					_id: "2",
+					name: "myA",
+					assoc: "15",
+					lowerBound: 1,
+				},
+				{
+					_id: "3",
+					name: "myA",
+					assoc: "15",
+					lowerBound: 1,
+				},
+			],
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//Class",
+			_id: "4",
+			name: "B",
+		},
+	],
+	types: [
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDVoid",
+			_id: "5",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDAny",
+			_id: "6",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDBoolean",
+			_id: "7",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDDouble",
+			_id: "8",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDInt",
+			_id: "9",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDLong",
+			_id: "10",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDString",
+			_id: "11",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDByte",
+			_id: "12",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDFloat",
+			_id: "13",
+		},
+		{
+			eClass: "http://cs.mcgill.ca/sel/cdm/1.0#//CDChar",
+			_id: "14",
+		},
+	],
+	associations: [
+		{
+			_id: "15",
+			name: "_A",
+			ends: ["2", "3"],
+			associationClass: "4",
+		},
+	],
+	layout: {
+		_id: "16",
+		containers: [
+			{
+				_id: "17",
+				key: "null",
+				value: [
+					{
+						_id: "18",
+						key: "1",
+						value: {
+							_id: "19",
+							x: 254.49988,
+							y: 352.0001,
+						},
+					},
+					{
+						_id: "20",
+						key: "4",
+						value: {
+							_id: "21",
+							x: 624.0,
+							y: 357.0,
+						},
+					},
+					{
+						_id: "22",
+						key: "15",
+						value: {
+							_id: "23",
+							x: 624.0,
+							y: 207.0,
+						},
+					},
+				],
+			},
+		],
+	},
+};
--- src/functionalities/CreateSvg.js
+++ src/functionalities/CreateSvg.js
@@ -1,9 +1,146 @@
+import { ONE_CLASS, TWO_CLASSES_WITH_ASSOCIATION } from "../assets/CDM_details";
+
+const NS = "http://www.w3.org/2000/svg";
+
 export default function CreateSvg() {
-	let ns = "http://www.w3.org/2000/svg";
-	let svg = document.createElementNS(ns, "svg");
-	svg.setAttribute("width", "100%");
-	svg.setAttribute("height", "200");
-	svg.style.backgroundColor = "blue";
-	document.body.appendChild(svg);
+	let svg = createSvg();
 	console.log("svg: ", svg);
+
+	/**
+	 * ONE_CLASS shows how one class is created alone
+	 * To check for two classes being created replace ONE_CLASS with TWO_CLASSES_WITH_ASSOCIATION
+	 */
+	jsonToSVG(svg, ONE_CLASS);
+}
+
+/**
+ *
+ * @param {The empty SVG canvas} svg
+ * @param {The json object that will be converted to an SVG} json
+ * This methods loops through the json object and creates the SVG representation
+ *
+ * !! This method is not complete yet !!
+ *
+ * Explanation:
+ * It loops through the object, when it reaches "classes", it loops through the different classes
+ * and created a box for each different class. For now it only works with one class, because we
+ * are giving the same coordinates for each box and so they are stacked and we can only see the last one created.
+ * (Can check console log to see that multiple classes are created).
+ * The class name comes from the "name" attribute of the class in the JSON object.
+ * A unique ID is generated for the class box and the className text field for easy manipulation.
+ * More IDs will be added later depending on where we need them.
+ *
+ * TODO: The next step is to give each box different coordinates so that they are not stacked.
+ * TODO: And then we can add the attributes and methods to each box.
+ *
+ * TODO: The next step after that is to add the associations between the classes.
+ *
+ */
+function jsonToSVG(svg, json) {
+	if (typeof json === "object") {
+		for (const key in json) {
+			if (key === "classes" && Array.isArray(json[key])) {
+				json[key].forEach((element, index) => {
+					console.log("element: ", element, index);
+					createClassBox(svg, element.name, index);
+				});
+			}
+		}
+	}
+}
+
+function createSvg() {
+	/**
+	 * Create SVG representation
+	 */
+	let svg = document.createElementNS(NS, "svg");
+	svg.setAttribute("width", "500");
+	svg.setAttribute("height", "200");
+	svg.setAttribute("viewBox", "0 0 100 100");
+	svg.style.backgroundColor = "grey";
+	document.body.appendChild(svg);
+	return svg;
+}
+
+function createClassBox(svg, name, index) {
+	/**
+	 * Create SVG group
+	 */
+	let g = document.createElementNS(NS, "g");
+	g.setAttribute("transform", "translate(-10, 20)");
+	g.setAttribute("id", "class-" + index); //? This is the id of the class
+	svg.appendChild(g);
+
+	/**
+	 * Create the class Name rectangle
+	 */
+
+	let path1 = document.createElementNS(NS, "path");
+	path1.setAttribute("d", "M 0 26 L 0 0 L 160 0 L 160 26");
+	path1.setAttribute("fill", "rgb(255, 255, 255)");
+	path1.setAttribute("stroke", "rgb(0, 0, 0)");
+	path1.setAttribute("stroke-miterlimit", "10");
+	path1.setAttribute("pointer-events", "all");
+	g.appendChild(path1);
+
+	let path3 = document.createElementNS(NS, "path");
+	path3.setAttribute("d", "M 0 26 L 160 26");
+	path3.setAttribute("fill", "none");
+	path3.setAttribute("stroke", "rgb(0, 0, 0)");
+	path3.setAttribute("stroke-miterlimit", "10");
+	path3.setAttribute("pointer-events", "none");
+	g.appendChild(path3);
+
+	/**
+	 * Create the class name text box for the first rectangle
+	 */
+
+	let g2 = document.createElementNS(NS, "g");
+	g2.setAttribute("fill", "rgb(0, 0, 0)");
+	g2.setAttribute("font-family", "Helvetica");
+	g2.setAttribute("font-weight", "bold");
+	g2.setAttribute("pointer-events", "none");
+	g2.setAttribute("font-size", "12");
+	g2.setAttribute("text-anchor", "middle");
+	g.appendChild(g2);
+
+	let className = document.createElementNS(NS, "text");
+	className.setAttribute("id", "className-" + index);
+	className.setAttribute("x", "79.5");
+	className.setAttribute("y", "17.5");
+	g2.appendChild(className);
+
+	var textNode = document.createTextNode(name);
+	className.appendChild(textNode);
+
+	/**
+	 * Create the attributes rectangle
+	 */
+	let path2 = document.createElementNS(NS, "path");
+	path2.setAttribute("d", "M 0 26 L 0 34 L 160 34 L 160 26");
+	path2.setAttribute("fill", "none");
+	path2.setAttribute("stroke", "rgb(0, 0, 0)");
+	path2.setAttribute("stroke-miterlimit", "10");
+	path2.setAttribute("pointer-events", "none");
+	g.appendChild(path2);
+
+	/**
+	 * TODO: Create attributes text box
+	 */
+
+	/**
+	 *  TODO: Create the methods Text box
+	 */
+
+	/**
+	 * Create the method rectangle for the class
+	 */
+
+	let path4 = document.createElementNS(NS, "path");
+	path4.setAttribute("d", "M 0 30 L 160 30");
+	path4.setAttribute("fill", "none");
+	path4.setAttribute("stroke", "rgb(0, 0, 0)");
+	path4.setAttribute("stroke-miterlimit", "10");
+	path4.setAttribute("pointer-events", "none");
+	g.appendChild(path4);
 }
